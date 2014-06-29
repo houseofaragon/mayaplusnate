f65c6375df4150667dc60651e812429b07d40747
karen <saintkaren@gmail.com>
2014-06-28 20:05:59 -0400

    possibly finished?

--- js/grayscale.js
+++ js/grayscale.js
@@ -18,40 +18,72 @@ var backgrounds = [
     'url(img/forest1.jpg) no-repeat center center scroll',
     'url(img/beachlands.jpg) no-repeat center center scroll',
     'url(img/black_beach.jpg) no-repeat center center scroll',
+    'url(img/eyes_and_skyways.jpg) no-repeat center center scroll',
+
     'url(img/broad_channel.jpg) no-repeat center center scroll',
     'url(img/complex.jpg) no-repeat center center scroll',
     'url(img/lights.jpg) no-repeat center center scroll',
     'url(img/marsh_owls.jpg) no-repeat center center scroll',
-    'url(img/beachlands.jpg)no-repeat center center scroll',
+
     'url(img/meadows.jpg) no-repeat center center scroll',
+    'url(img/nate_field.jpg) no-repeat center center scroll',
+    'url(img/night_trees.jpg) no-repeat center center scroll',
+    'url(img/snowfog_silhouette.jpg) no-repeat center center scroll',
+
     'url(img/sea_trees.jpg) no-repeat center center scroll',
     'url(img/projection_house.jpg) no-repeat center center scroll',
-    'url(img/snow_hill.jpg) no-repeat center center scroll'
+    'url(img/snow_hill.jpg) no-repeat center center scroll',
+    'url(img/staten_wallpaper.jpg) no-repeat center center scroll',
+
+    'url(img/mural.jpg) no-repeat center center scroll',
+    'url(img/natebushes.jpg) no-repeat center center scroll',
+    'url(img/photography.jpg) no-repeat center center scroll',
+    'url(img/pool.jpg) no-repeat center center scroll',
+    'url(img/vermontroads.jpg) no-repeat center center scroll'
+
+
 ];
 
-function randomInt(min, max) {
-    return Math.floor(Math.random() * 13);
+function randomInt() {
+    return Math.floor(Math.random() * 20);
 }
 
 var current = 0;
 var max = 11;
+var previous;
 $('#about').on('click', function(){
+    current = randomInt();
+    console.log('Current: ' + current);
     $('#about').text('');
-    if(current > max){
-        current = 0;
+    if(current === previous){
+        current = previous + 1;
     }
 
     $('#about .row').hide();
     $('#about').css('background',backgrounds[current]);
     $('#about').css('background-size','100% 100%');
 
-    if( current % 2 ){
-        $('#about').append('<h2>6:00PM</h2>');
-    } else if(current%3){
-        $('#about').append('<h2>September 14, 2014</h2>');
-    } else if(current%5){
-        $('#about').append('<h2>Milk & Roses</h2>');
-    }
+    if( current === 2 || current === 6){
+        $('#about').append('<h1 class="brand-heading">Maya + Nate</h1>');
+    } else if(current === 3 || current === 7){
+        $('#about').append('<h1 class="brand-heading">6:00 PM</h1>');
+    } else if( current === 4 || current === 9){
+        $('#about').append('<h1 class="brand-heading">Milk & roses</h1>');
+    } else if(current === 5 || current === 10){
+        $('#about').append('<h1 class="brand-heading">September 14 2014</h1>');
+    } else if(current === 11 || current === 12){
+        $('#about').append('<h1 class="brand-heading">CAMPING! </h1>');
+    } else if(current === 0 || current === 13){
+        $('#about').append('<h1 class="brand-heading">SATURDAY</h1>');
+    }   else if(current === 1 || current === 14){
+        $('#about').append('<h1 class="brand-heading">NATE + MAYA</h1>');
+    } 
+    else{
+        $('#about').append('<h1 class="brand-heading">Cold springs</h1>');
 
-    current = current + 1;
+    }
+    $('#about').css('#about h1','margin-top:50%');
+
+    //current = current + 1;
+    previous = current;
 });
